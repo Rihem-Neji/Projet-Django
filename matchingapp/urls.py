95717f315b099313fd36794338d3ca787d658a07
Rihem-Neji <rihem.neji0123@gmail.com>
2025-07-30 13:25:51 +0100

    Redeploiement clean pour resoudre 400

--- matchingapp/urls.py
+++ matchingapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+def home_redirect(request):
+    return redirect('formulaire')
 
 urlpatterns = [
+    path('', home_redirect),  # redirige la racine vers /formulaire/
     path('formulaire/', views.formulaire, name='formulaire'),
     path('result/', views.resultat, name='result'),
 ]
